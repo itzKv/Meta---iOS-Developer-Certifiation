068cc81edf46f5a7c5156db462d201f8005db9a9
Kevin Brivio <116947826+itzKv@users.noreply.github.com>
2024-05-04 22:20:58 +0700

    On Progress
    Learnt about Variables and Operators in Swift

---
+++ Programming Fundamentals in Swift/Introduction to programming in Swift/Exercise-constant-vs-variables.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+let day = "Monday"
+let dailyTemperature = "75"
+
+print("Today is \(day). Rise and shine!")
+print("The temperature on \(day) is \(dailyTemperature)ºF")
+
+var temperature = 70
+temperature = 80
+print("The temperature on \(day) morning is \(temperature)ºF")
+
+let weeklyTemperature = 90
+temperature = weeklyTemperature
+print("The temperature on this week is \(temperature)ºF")
