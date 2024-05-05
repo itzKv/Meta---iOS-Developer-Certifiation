df379b785ae223aad009190cfd9754699977d15a
itzKv <kenkenku6@gmail.com>
2024-05-05 16:33:36 +0700

    Completed

--- Programming Fundamentals in Swift/Introduction to programming in Swift/Switch-Statement.playground/Contents.swift
+++ Programming Fundamentals in Swift/Introduction to programming in Swift/Switch-Statement.playground/Contents.swift
@@ -1,3 +1,9 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let language = "English"
+switch language {
+    case "English": print("The default language is set to English.")
+    case "Spanish": print("The default language is set to Spanish.")
+    default: print("The default language has not been configured yet.")
+    // default: break
+}
